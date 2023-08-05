cf867521aecd3e637ed5b5060a77b5c04bb1b52f
Your Name <22kartiksanil@gmail.com>
2023-08-06 01:03:19 +0530

    Create temperature_converter.py

---
+++ Hello World/temperature_converter.py
@@ -0,0 +1,9 @@
+# Celcius to Farenheit 
+celcius=float(input('Enter the temperature:'))
+farenheit=(celcius * 1.8)+32
+print('Farenheit:', farenheit)
+
+# Farenheit to Celcius
+farenheit=float(input('Enter the temperature:'))
+celcius=(farenheit - 32)/1.8
+print('Celcius:', celcius)
